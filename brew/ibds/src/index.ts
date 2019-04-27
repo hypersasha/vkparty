d6e5d4f07cc404ce0d34914b2d5c8b24bfe7cdd7
allenWTF <niowen@mail.ru>
2019-04-27 23:45:12 +0300

    ssl test

--- brew/ibds/src/index.ts
+++ brew/ibds/src/index.ts
@@ -1,8 +1,15 @@
 import app from "./NetworkLayer";
 import * as dotenv from "dotenv";
-const PORT = 80;
+import * as https from "https";
+import * as fs from "fs";
+
+const PORT = 3000;
 
 dotenv.config();
-app.listen(PORT, () => {
-    console.log("Jeoparty server started on port " + PORT);
+https.createServer({
+    key: fs.readFileSync('./private.key'),
+    cert: fs.readFileSync('./certificate.crt'),
+    passphrase: 'coldmove'
+}, app).listen(443, () => {
+    console.log("server started")
 });
