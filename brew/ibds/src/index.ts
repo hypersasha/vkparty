769837d11c03b6e15176b9fc02c6720f81ef6919
allenWTF <niowen@mail.ru>
2019-05-06 18:56:01 +0300

    Socket.io test

--- brew/ibds/src/index.ts
+++ brew/ibds/src/index.ts
@@ -2,14 +2,24 @@ import app from "./NetworkLayer";
 import * as dotenv from "dotenv";
 import * as https from "https";
 import * as fs from "fs";
+import * as SocketIO from "socket.io";
 
 const PORT = 443;
 
 dotenv.config();
-https.createServer({
+let server = https.createServer({
     key: fs.readFileSync('./private.key'),
     cert: fs.readFileSync('./certificate.crt'),
     passphrase: 'coldmove'
 }, app).listen(PORT, () => {
     console.log("VK Rave Server started on port " + PORT)
+});
+
+let io = require("socket.io")(server);
+
+
+io.on('connection', (socket : any) => {
+   socket.on('ping', () => {
+      socket.send('pong');
+   });
 });
